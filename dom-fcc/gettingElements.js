075cff0a7136422ff063e28b175159559fbb2a96
AbdullahAliCodes <abdullahali.dullz@gmail.com>
2026-04-03 10:55:43 +0200

    started dom practice

---
+++ dom-fcc/gettingElements.js
@@ -0,0 +1,21 @@
+// DOM Manipulation
+
+// GetElementById()
+const title = document.getElementById("main-heading");
+console.log(title);
+
+// GetElementByClass();
+const listItem = document.getElementsByClassName("list-items");
+console.log(listItem);
+
+// getElementsByTagName()
+const listItems = document.getElementsByTagName("li");
+console.log(listItems);
+
+// querySelector()
+const container = document.querySelector("div");
+console.log(container);
+
+// querySelectorAll()
+const allContainers = document.querySelector("div");
+console.log(allContainers);
